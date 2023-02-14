e4991a42b050c14b44197284076ad6c10efc519b
emicludo <emg@cludo.com>
2023-02-15 00:43:47 +0100

    clean up of node app and adds readme

--- itu-minitwit-node/routes/index.js
+++ itu-minitwit-node/routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-const database = require('../db/dbService')
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
