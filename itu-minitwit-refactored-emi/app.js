c17332e7767d920046150d0bc30b901804bbc32f
emicludo <emg@cludo.com>
2023-02-14 15:33:27 +0100

    change users route

--- itu-minitwit-refactored-emi/app.js
+++ itu-minitwit-refactored-emi/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var messageRouter = require('./routes/message');
 
 //Database Service
 const db = require('./db/dbService');
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/message', messageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,7 +37,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- itu-minitwit-refactored-emi/routes/index.js
+++ itu-minitwit-refactored-emi/routes/index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const database = require('../db/dbService')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/users', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
