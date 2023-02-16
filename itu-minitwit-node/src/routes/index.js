32dc86e58cede77dcae227beff5f8a887e35921b
Melissa Høegh Marcher <mhom@itu.dk>
2023-02-16 16:28:33 +0100

    index changed, not done

--- itu-minitwit-node/src/routes/index.js
+++ itu-minitwit-node/src/routes/index.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * GET /
+ *
+ * Checks whether the user is logged in.
+ * If not logged in, retrieves most recent messages from the database and returns them (public timeline).
+ * If logged in, retrieves most recent messages (follower and own) from the database and returns them.
+ * 
+ * Errors:
+ *  - 500: An error occurred while retrieving the message
+ */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ITU-MiniTwit-App' });
+  if(req.params.session.user_id == null){ // if not logged in 
+
+    database.all("select message.*, user.* from message, user \
+                where message.flagged = 0 \
+                and message.author_id = user.user_id \
+                order by message.pub_date desc limit ?"
+    , [PER_PAGE], (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).render('error');
+      return;
+    }
+
+    console.log('Successfully retrieved ' + rows.length + ' messages');
+    res.render('index', { title: 'ITU-MiniTwit-App' });
+    });
+
+  } else { // if logged in 
+
+    database.all("select message.*, user.* from message, user \
+                  where message.flagged = 0 \
+                  and message.author_id = user.user_id \
+                  and ( user.user_id = ? or user.user_id \
+                    in (select whom_id from follower \
+                    where who_id = ?)) \
+                  order by message.pub_date desc limit ?"
+    , [PER_PAGE], (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).render('error');
+      return;
+    }
+
+    console.log('Successfully retrieved ' + rows.length + ' messages');
+    res.render('index', { title: 'ITU-MiniTwit-App' });
+    });
+
+  }
 });
+
 
 module.exports = router;
