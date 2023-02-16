0495364bd0b69e3770f2a988342ac3ed98e0250e
Ingrid <inch@itu.dk>
2023-02-16 16:30:35 +0100

    quick routing fix

--- itu-minitwit-node/src/routes/authentication.js
+++ itu-minitwit-node/src/routes/authentication.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('authentication');
-});
-
-router.get('/', function(req, res, next) {
-  res.render('login');
 });
 
 module.exports = router;
