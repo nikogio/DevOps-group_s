734740a8e81069dbedf53418f8d28ea988bd0fe8
Matej-Basta <matej.basta@email.cz>
2023-02-23 16:56:03 +0100

    getting signin error

--- itu-minitwit-node/src/routes/signin.js
+++ itu-minitwit-node/src/routes/signin.js
@@ -1,20 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const database = require('../db/dbService')
+
 router.get('/', function(req, res, next) {
-  res.render('signin');
+
+  const errorMessage = req.session.errorMessage;
+
+  delete req.session.errorMessage;
+  console.log(errorMessage);
+  res.render('signin', {errorMessage: errorMessage});
 });
 
+
 router.post('/', function (req, res, next) {
-  database.all("SELECT * FROM user WHERE username = ?", req.body.username, (err, rows) => {
+
+  database.all('SELECT * FROM user WHERE username = ?', req.body.username, (err, rows) => {
     
     if (err) {
       console.error(err);
       res.status(500).send({ error: 'An error occurred while retrieving user', description: err.toString() });
+      
       return;
     }
 
-    console.log(rows);
+    // if user does not exist
+    if (rows.length == 0) {
+      req.session.errorMessage = 'Incorrect username or password';
+      res.redirect('/api/signin');
+      return;
+    }
+
+    res.redirect('/api/');
   })
 })
 
