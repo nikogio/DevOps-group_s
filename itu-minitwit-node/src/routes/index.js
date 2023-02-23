0ed907f39ee0dc1a167bc313c969eb8d441a41ac
Ingrid <inch@itu.dk>
2023-02-23 14:49:12 +0100

    added sign up + cleanup

--- itu-minitwit-node/src/routes/index.js
+++ itu-minitwit-node/src/routes/index.js
@@ -22,19 +22,15 @@ router.get('/', function(req, res, next) {
                 and message.author_id = user.user_id \
                 order by message.pub_date desc limit 30"
     , [], (err, rows) => {
-      console.log(rows);
 
     if (err) {
       console.error(err);
       res.status(500).render('error');
       return;
     }
-
-    var d = new Date((rows[0].pub_date)*1000);
-    console.log("HALLOHALLIHERERDENJKSHDKJAHLKJDH: " + d);
     
     console.log('Successfully retrieved ' + rows.length + ' messages');
-    res.render('index', { title: 'MiniTwit', messages: rows });
+    res.render('index', { messages: rows });
     });
 
 
