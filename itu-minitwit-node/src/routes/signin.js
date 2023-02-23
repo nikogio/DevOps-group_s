dcfa2d4a6972fc1665e74bae218837affc082b5a
Matej-Basta <matej.basta@email.cz>
2023-02-23 15:37:36 +0100

    signin preparation

--- itu-minitwit-node/src/routes/signin.js
+++ itu-minitwit-node/src/routes/signin.js
@@ -5,4 +5,17 @@ router.get('/', function(req, res, next) {
   res.render('signin', { title: 'MiniTwit' });
 });
 
+router.post('/', function (req, res, next) {
+  database.all("SELECT * FROM user WHERE username = ?", req.body.username, (err, rows) => {
+    
+    if (err) {
+      console.error(err);
+      res.status(500).send({ error: 'An error occurred while retrieving user', description: err.toString() });
+      return;
+    }
+
+    console.log(rows);
+  })
+})
+
 module.exports = router;
