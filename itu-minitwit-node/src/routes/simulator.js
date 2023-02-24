fd41140833bd4bb8792cc024d825e412dc21ae4c
emicludo <emg@cludo.com>
2023-02-24 10:25:09 +0100

    add POST /msgs/:username

--- itu-minitwit-node/src/routes/simulator.js
+++ itu-minitwit-node/src/routes/simulator.js
@@ -74,42 +74,6 @@ router.post("/register", async function (req, res, next) {
   }
 });
 
-// @app.route("/register", methods=["POST"])
-// def register():
-//     # update LATEST
-//     update_latest(request)
-
-//     request_data = request.json
-
-//     error = None
-//     if request.method == "POST":
-//         if not request_data["username"]:
-//             error = "You have to enter a username"
-//         elif not request_data["email"] or "@" not in request_data["email"]:
-//             error = "You have to enter a valid email address"
-//         elif not request_data["pwd"]:
-//             error = "You have to enter a password"
-//         elif get_user_id(request_data["username"]) is not None:
-//             error = "The username is already taken"
-//         else:
-//             query = """INSERT INTO user
-//                        (username, email, pw_hash) VALUES (?, ?, ?)"""
-//             g.db.execute(
-//                 query,
-//                 [
-//                     request_data["username"],
-//                     request_data["email"],
-//                     generate_password_hash(request_data["pwd"]),
-//                 ],
-//             )
-//             g.db.commit()
-
-//     if error:
-//         return jsonify({"status": 400, "error_msg": error}), 400
-//     else:
-//         return "", 204
-
-
 router.get('/msgs', function (req, res, next) {
   try {
     //Checks if header comes from simulator
@@ -207,6 +171,48 @@ router.get('/msgs/:username', async function (req, res, next) {
         res.status(204).send("");
       } else {
         res.status(200).send(filteredMsgs);
+      }
+    });
+  } catch (error) {
+    console.log("error", error)
+  }
+})
+
+router.post('/msgs/:username', async function (req, res, next) {
+  let username = req.params.username;
+  let content = req.body.content;
+  try {
+    //Checks if header comes from simulator
+    const header = req.headers.authorization;
+    if (!isSimulator(header)) {
+      res.status(403).send({ status: 403, error_msg: "You are not authorized to use this resource!" });
+      return;
+    }
+    //Updates Latest
+    var latest = req.query.latest;
+    if (latest !== undefined && parseInt(latest) !== NaN) {
+      latestService.updateLatest(parseInt(latest));
+    }
+
+    const users = await getAllUsers()
+    const userSelected = users.find(user => user.username = username)
+    if (userSelected == -1) {
+      res.status(404).send({ status: 404, error_msg: "User is not on our database" });
+    }
+    const userId = userSelected.user_id
+
+    const body = {
+      author_id: userId,
+      text: content,
+      pub_date: Math.floor(Date.now() / 1000),
+      flagged: 0
+    };
+    database.add('message', body, function(lasdId, err) {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log('New message added successfully with id: ' + lasdId);
+        res.status(204).send("");
       }
     });
   } catch (error) {
