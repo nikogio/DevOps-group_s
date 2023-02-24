fbf8d6479c5274261773aa2d837ceb5f77aee7e8
Matej-Basta <matej.basta@email.cz>
2023-02-24 20:26:55 +0100

    changing the timeline headline

--- itu-minitwit-node/src/routes/index.js
+++ itu-minitwit-node/src/routes/index.js
@@ -17,9 +17,39 @@ const database = require('../db/dbService')
 
 // TODO: Switch to "personal" timeline if logged in. Currently only shows public timeline. 
 router.get('/', function(req, res, next) {
-    const flash = req.session.flash;
-    delete req.session.flash;
-    database.all("select message.*, user.* from message, user \
+
+  if (!req.session.user) {
+    res.redirect('/api/public');
+  }
+  
+  const flash = req.session.flash;
+  delete req.session.flash;
+
+  database.all("select message.*, user.* from message, user \
+                where message.flagged = 0 \
+                and message.author_id = user.user_id \
+                and (user.user_id = ? or user.user_id in (select whom_id from follower where who_id = ?)) \
+                order by message.pub_date desc limit 30"
+    , [req.session.user, req.session.user], (err, rows) => {
+
+    if (err) {
+      console.error(err);
+      res.status(500).render('error');
+      return;
+    }
+    
+    console.log('Successfully retrieved ' + rows.length + ' messages');
+    console.log(rows);
+    res.render('index', { messages: rows, flash: flash, path: req.path});
+    });
+  
+    
+});
+
+/* Displays the latest messages of all users. */
+router.get('/public', function (req, res, next) {
+  
+  database.all("select message.*, user.* from message, user \
                 where message.flagged = 0 \
                 and message.author_id = user.user_id \
                 order by message.pub_date desc limit 30"
@@ -32,14 +62,8 @@ router.get('/', function(req, res, next) {
     }
     
     console.log('Successfully retrieved ' + rows.length + ' messages');
-    res.render('index', { messages: rows , flash: flash});
+    res.render('index', { messages: rows, path: req.path});
     });
-});
-
-
-router.post('/register', function(req, res, next) {
-
-    res.render('test', { title: 'MiniTwit', messages: "test" });
 });
 
 module.exports = router;
