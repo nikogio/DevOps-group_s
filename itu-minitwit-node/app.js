06fac3248996d700ed8092b3b4868f8172e7fc36
Nicholas Gioachini <93983535+nikogio@users.noreply.github.com>
2023-02-26 21:44:18 +0100

    Revert "W4 add endpoints for simulator"

--- itu-minitwit-node/app.js
+++ itu-minitwit-node/app.js
@@ -35,6 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/api/message', messageRouter);
 app.use('/api/follow', followRouter);
 app.use('/api/unfollow', unfollowRouter);
--- itu-minitwit-node/src/utils/authorizationValidator.js
+++
@@ -1,5 +0,0 @@
-const isSimulator = (header) => {
-  return header === 'Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh';
-}
-
-module.exports = isSimulator;
